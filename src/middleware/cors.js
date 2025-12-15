dc388976d6ddf146b7a41d3ceec9b738682c9c66
smmxk <simmaxka93@icloud.com>
2025-12-15 15:35:00 +0100

    Add CORS middleware with configurable origins

---
+++ src/middleware/cors.js
@@ -0,0 +1,19 @@
+const cors = require('cors');
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+module.exports = cors(corsOptions);
